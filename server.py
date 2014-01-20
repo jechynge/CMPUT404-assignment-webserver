4d5eeb95cde014fd790dae96c9b679595a118ef4
Jordan Ching <ching.jordan@gmail.com>
2014-01-19 23:01:33 -0700

    Added better security and comments. Removed print statements.

--- server.py
+++ server.py
@@ -34,19 +34,28 @@ class MyWebServer(SocketServer.BaseRequestHandler):
     okResponseHeader = "HTTP/1.1 200 OK"
     badRequestResponse = "HTTP/1.1 400 Bad Request\nContent-Length:145\nContent-Type:text/html;\n\n<!DOCTYPE html>\n<html><head><title>Bad Request</title></head><body><h1>400: Bad Request</h1>This server only supports GET requests.</body></html>"
     notFoundResponse = "HTTP/1.1 404 Not Found\nContent-Length:154\nContent-Type:text/html;\n\n<!DOCTYPE html>\n<html><head><title>Page Not Found</title></head><body><h1>404: Page Not Found</h1>The page you requested could not be found.</body></html>"
+    serverDirectory = ""
+    
+    def __init__(self, request, client_address, server):
+        self.serverDirectory = re.escape(os.path.dirname(os.path.abspath(__file__)) + '/www/')
+        SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
+    
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
         headers = self.data.split("\n")
         request = headers[0].split(" ")
+        
+        # Make sure the request is a get. Otherwise, let the client know we
+        # can't handle their request; return 400
         if(request[0] == "GET"):
-            print ("{"+request[1]+"}")
             response = self.handleGet(request[1])
         else:
             self.request.sendall(self.badRequestResponse)
             return
         
+        # If None is returned instead of a file, assume file doesn't exist;
+        # return 404.
         if(response is None):
             self.request.sendall(self.notFoundResponse)
             return
@@ -56,9 +65,19 @@ class MyWebServer(SocketServer.BaseRequestHandler):
         
     def handleGet(self,requestedFile):
         requestedFile = "www" + requestedFile
+        
+        # If only a directory was specified, assume 'index.html' was intended
         if(re.match('.*\/$', requestedFile)):
             requestedFile += "index.html"
-            
+        
+        # Get the absolute path of the requested file.
+        absFilePath = os.path.abspath(requestedFile)
+        
+        # Ensure that the requested file is in the 'www' directory
+        if(not re.match('^'+self.serverDirectory+'.*',absFilePath)):
+            return None
+        
+        # If the file exists, open and read it. Otherwise, return None
         if(os.path.isfile(requestedFile)):
             contentFile = open(requestedFile, 'r')
             content = contentFile.read()
@@ -66,6 +85,7 @@ class MyWebServer(SocketServer.BaseRequestHandler):
         else:
             return None
         
+        # Build HTTP headers
         contentLength = len(content)
         response = self.okResponseHeader
         response += "\nContent-Length: " + str(contentLength)
@@ -76,13 +96,10 @@ class MyWebServer(SocketServer.BaseRequestHandler):
         else:
             return None
         
+        # Attach body to headers
         response += "\n\n" + content
         
         return response
-        
-    def sendReply(self, header, body):
-        contentLength = len(body)
-        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
